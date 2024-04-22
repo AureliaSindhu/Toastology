9a843dd2102b2d26b92950259ee4769b6a7c0dbf
Aurelisa Sindhunirmala <aurelisajuans@gmail.com>
2024-04-22 11:05:24 -0700

    Update Files

--- script.js
+++ script.js
@@ -8,47 +8,47 @@ function startQuiz() {
 
   const ep = [
     [
-        "You approach the counter, torn between two options",
-        "With your chosen toast in hand, you’re faced with another decision",
-        "As you’re heading home after enjoying your delicious meal, you stop by the bakery's bulletin board and spot a flyer. It announces a baking workshop that is coming up soon."
+      "You approach the counter, torn between two options",
+      "With your chosen toast in hand, you’re faced with another decision",
+      "As you’re heading home after enjoying your delicious meal, you stop by the bakery's bulletin board and spot a flyer. It announces a baking workshop that is coming up soon."
     ],
     [
-        "You spot a street performer wowing everyone with their amazing music.",
-        "As you make your way through the city, you stumble upon a vibrant mural painted on a building, each stroke telling a tale.",
-        "As your travel, you’re immediately drawn to the rows of books lining the shelves."
+      "You spot a street performer wowing everyone with their amazing music.",
+      "As you make your way through the city, you stumble upon a vibrant mural painted on a building, each stroke telling a tale.",
+      "As your travel, you’re immediately drawn to the rows of books lining the shelves."
     ],
     [
-        "Looking around your living room, you see your shelves filled with books and photos from your past adventures.",
-        "As you settle down for the night, you perk up at the sound of music playing softly from the speakers.",
-        "Sitting at your desk, you notice a blank canvas and a set of paints from across the room. You remember you've been wanting to paint whenever you have some free time."
+      "Looking around your living room, you see your shelves filled with books and photos from your past adventures.",
+      "As you settle down for the night, you perk up at the sound of music playing softly from the speakers.",
+      "Sitting at your desk, you notice a blank canvas and a set of paints from across the room. You remember you've been wanting to paint whenever you have some free time."
     ],
     [
-        "Glancing at your planner, you’re reminded of the tasks that await you tomorrow.",
-        "Before you can lie on your bed, you notice a pile of laundry waiting to be folded in the corner of the room.",
-        "As you drift off to sleep, you think about your plans for the next morning."
+      "Glancing at your planner, you’re reminded of the tasks that await you tomorrow.",
+      "Before you can lie on your bed, you notice a pile of laundry waiting to be folded in the corner of the room.",
+      "As you drift off to sleep, you think about your plans for the next morning."
     ]
   ];
 
   const options = [
   [
-      ["You ask the employee for today's special", "Quietly checking out the toast options on your own"],
-      ["You write a note on the post-it wall in the bakery", "Finding a cozy corner to enjoy your snack in peace"],
-      ["You sign up for the workshop", "You snap a photo of the flyer and consider it later"]
+    ["You ask the employee for today's special", "Quietly checking out the toast options on your own"],
+    ["You write a note on the post-it wall in the bakery", "Finding a cozy corner to enjoy your snack in peace"],
+    ["You sign up for the workshop", "You snap a photo of the flyer and consider it later"]
+],
+  [
+    ["Drawn to the beauty of the music, you pause to immerse yourself in the moment", "Preferring to stay focused on your destination, you continue on your way"],
+    ["Captivated by the vibrant colors, you decide to take a picture of the mural", "Appreciating it briefly before moving on with your day"],
+    ["Intrigued, you decide to enter the bookstore to explore its treasures.", "Feeling torn between curiosity and staying on track, you decide to make a note of the bookstore’s location to return later"]
   ],
   [
-      ["Drawn to the beauty of the music, you pause to immerse yourself in the moment", "Preferring to stay focused on your destination, you continue on your way"],
-      ["Captivated by the vibrant colors, you decide to take a picture of the mural", "Appreciating it briefly before moving on with your day"],
-      ["Intrigued, you decide to enter the bookstore to explore its treasures.", "Feeling torn between curiosity and staying on track, you decide to make a note of the bookstore’s location to return later"]
+    ["You find yourself reaching for a book from the shelf to read and relax", "You reflect on the memories that cherish your moments and experiences"],
+    ["Intrigued by the verses and melodies, you find yourself analyzing the meaning behind the music", "You allow yourself to be swept away by the music as you dance around the living room"],
+    ["You find yourself drawn to planning out your painting beforehand", "You follow your heart and express your freedom, embracing spontaneity and experimentation"]
   ],
   [
-      ["You find yourself reaching for a book from the shelf to read and relax", "You reflect on the memories that cherish your moments and experiences"],
-      ["Intrigued by the verses and melodies, you find yourself analyzing the meaning behind the music", "You allow yourself to be swept away by the music as you dance around the living room"],
-      ["You find yourself drawn to planning out your painting beforehand", "You follow your heart and express your freedom, embracing spontaneity and experimentation"]
-  ],
-  [
-      ["Feeling a sense of satisfaction in organization, you review your schedule and prioritize your to-do list for the next day", "You set it aside for the next day"],
-      ["You went to work directly and folded your laundry", "Prioritizing relaxation and leisure, you decide to leave the laundry for the next day"],
-      ["Wanting to start the day with productivity, you set an alarm", "You want to wake up naturally and trust that you can and will wake up at your own pace"]
+    ["Feeling a sense of satisfaction in organization, you review your schedule and prioritize your to-do list for the next day", "You set it aside for the next day"],
+    ["You went to work directly and folded your laundry", "Prioritizing relaxation and leisure, you decide to leave the laundry for the next day"],
+    ["Wanting to start the day with productivity, you set an alarm", "You want to wake up naturally and trust that you can and will wake up at your own pace"]
   ]
   ];
 
